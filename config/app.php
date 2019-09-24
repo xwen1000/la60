e8803b0510fb1eb813e7e4f7e1acfb5bbc97379d
xingwen <xwen100@qq.com>
2019-09-24 11:55:20 +0800

    laravel-admin,配置修改

--- config/app.php
+++ config/app.php
@@ -80,7 +80,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'zh-CN',
 
     /*
     |--------------------------------------------------------------------------
@@ -225,6 +225,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'setEventDispatcher' => Illuminate\Events\Dispatcher::class,
 
     ],
 
