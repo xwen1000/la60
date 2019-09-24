1b640dbd6138791e9e52f83918b3096c08c89e59
xingwen <xwen100@qq.com>
2019-09-25 05:25:19 +0800

    article模型

---
+++ database/migrations/2019_09_24_205433_create_articles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('author_id')->default(0);
+            $table->string('title')->default('');
+            $table->text('content')->nullable();
+            $table->unsignedInteger('rate')->default(0);
+            $table->timestamp('release_at');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
